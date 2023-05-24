cb5717128d001929062e61993e91bcb1f00a2cd2
Sarah <sarahdavies186@gmail.com>
2023-05-24 15:58:55 +0100

    repeated same steps with class component

--- user-card/src/App.jsx
+++ user-card/src/App.jsx
@@ -2,11 +2,18 @@ import { useState } from "react";
 import "./App.css";
 import user from "./user.json";
 import UserCard from "./UserCard";
+import { UserCardClass } from "./UserCardClass";
 
 function App() {
   return (
     <>
       <UserCard
+        name={user.name}
+        phoneNumber={user.phoneNumber}
+        age={user.age}
+        address={user.address}
+      />
+      <UserCardClass
         name={user.name}
         phoneNumber={user.phoneNumber}
         age={user.age}
--- user-card/src/UserCard.jsx
+++ user-card/src/UserCard.jsx
@@ -1,13 +1,13 @@
 export default function UserCard({ name, phoneNumber, age, address }) {
   return (
-    <div class="card">
-      <h2 class="name">{name}</h2>
-      <div class="body">
-        <div class="label">Age:</div>
+    <div className="card">
+      <h2 className="name">{name}</h2>
+      <div className="body">
+        <div className="label">Age:</div>
         <div>{age}</div>
-        <div class="label">Phone:</div>
+        <div className="label">Phone:</div>
         <div>{phoneNumber}</div>
-        <div class="label">Address:</div>
+        <div className="label">Address:</div>
         <div>{address}</div>
       </div>
     </div>
