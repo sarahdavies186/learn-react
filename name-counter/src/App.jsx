2a066b7aad051a6db03541e85021748324d0f876
Sarah <sarahdavies186@gmail.com>
2023-05-24 23:36:02 +0100

    function to show name from user input and increase or decrease age

--- name-counter/src/App.jsx
+++ name-counter/src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+
+  const handleUpdateName = (event) => {
+    setName(event.target.value);
+  }
+
+  const incrementAge = () => {
+    setAge(currentAge => currentAge + 1)
+  }
+
+  const decrementAge = () => {
+    setAge(currentAge => currentAge - 1)
+  }
 
   return (
     <>
-    
+      <input type="text" value={name} onChange={handleUpdateName} />
+      <button type="submit">Submit</button>
+      <br />
+      <button onClick={decrementAge}>-</button>
+      {age}
+      <button onClick={incrementAge}>+</button>
+      <br />
+      My name is {name} and I am {age} years old
     </>
-  )
+  );
 }
 
-export default App
+export default App;
