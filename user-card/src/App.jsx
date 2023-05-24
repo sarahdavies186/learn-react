902aa1fccfcded9543b189d181acbe7b414a6258
Sarah <sarahdavies186@gmail.com>
2023-05-24 15:46:59 +0100

    passing user info to UserCard from app.jsx

--- user-card/src/App.jsx
+++ user-card/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import user from "./user.json";
+import UserCard from "./UserCard";
 
 function App() {
-
   return (
     <>
-    
+      <UserCard
+        name={user.name}
+        phoneNumber={user.phoneNumber}
+        age={user.age}
+        address={user.address}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
