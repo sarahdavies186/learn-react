c12e7ca2c846437ffa8e2b2c6c39cfa9d3d25555
Sarah <sarahdavies186@gmail.com>
2023-05-24 17:10:57 +0100

    function to remove first element of array

--- array-component/src/App.jsx
+++ array-component/src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+const INITIAL_VALUE = ["A", "B", "C"];
 
 function App() {
+  const [array, setArray] = useState(INITIAL_VALUE);
+
+  function removeFirstElement() {
+    setArray((currentArray) => {
+      return currentArray.slice(1);
+    });
+  }
+
   return (
     <>
-   
+      <button onClick={removeFirstElement}>Remove First Element</button>
+      {array.join(", ")}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
