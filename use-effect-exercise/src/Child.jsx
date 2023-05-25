d602846e9260b5565bebdf0bdbd600d153e56855
Sarah <sarahdavies186@gmail.com>
2023-05-25 20:21:24 +0100

    setup for exercise and console log each time component re-renders

---
+++ use-effect-exercise/src/Child.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+export function Child() {
+  const [age, setAge] = useState(0)
+  const [name, setName] = useState("")
+
+  useEffect(() => {
+    console.log('render')
+  })
+
+  return (
+    <div>
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      <br />
+      <br />
+      <button onClick={() => setAge(a => a - 1)}>-</button>
+      {age}
+      <button onClick={() => setAge(a => a + 1)}>+</button>
+      <br />
+      <br />
+      My name is {name} and I am {age} years old.
+    </div>
+  )
+}
