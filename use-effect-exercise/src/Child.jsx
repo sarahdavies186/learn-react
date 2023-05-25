6da3042f0670990d103bce669d96f6048095c783
Sarah <sarahdavies186@gmail.com>
2023-05-25 20:47:08 +0100

    console log when component unmounts

--- use-effect-exercise/src/Child.jsx
+++ use-effect-exercise/src/Child.jsx
@@ -1,37 +1,45 @@
-import { useEffect } from "react"
-import { useState } from "react"
+import { useEffect } from "react";
+import { useState } from "react";
 
 export function Child() {
-  const [age, setAge] = useState(0)
-  const [name, setName] = useState("")
+  const [age, setAge] = useState(0);
+  const [name, setName] = useState("");
 
   useEffect(() => {
-    console.log('render')
-  })
+    console.log("render");
+  });
 
   useEffect(() => {
-    console.log('Hi')
-  }, [])
+    console.log("Hi");
+
+    return () => {
+      console.log("Bye");
+    };
+  }, []);
 
   useEffect(() => {
-    console.log(`My name is ${name} and I am ${age} years old.`)
-  }, [name, age])
+    console.log(`My name is ${name} and I am ${age} years old.`);
+  }, [name, age]);
 
   useEffect(() => {
-    document.title = name
-  }, [name])
+    document.title = name;
+  }, [name]);
 
   return (
     <div>
-      <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <br />
       <br />
-      <button onClick={() => setAge(a => a - 1)}>-</button>
+      <button onClick={() => setAge((a) => a - 1)}>-</button>
       {age}
-      <button onClick={() => setAge(a => a + 1)}>+</button>
+      <button onClick={() => setAge((a) => a + 1)}>+</button>
       <br />
       <br />
       My name is {name} and I am {age} years old.
     </div>
-  )
+  );
 }
