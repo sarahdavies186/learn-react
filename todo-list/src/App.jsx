5c83f5626f5e019dec7aba8c40a07292b7a4f200
Sarah <sarahdavies186@gmail.com>
2023-05-26 17:42:14 +0100

    function to display new todo items when added in input

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
-import './styles.css'
+import { useState } from "react";
+import "./styles.css";
+import Todo from "./Todo";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("")
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id:crypto.randomUUID(), message: newTodo, completed: false }
+      ]
+    })
+  }
+
+  // console.log(todos);
 
   return (
     <>
-      <input type='text'/>
+      <form id="new-todo-form" onSubmit={handleSubmit}>
+        <input type="text" id="todo" placeholder="add todo" value={newTodo} onChange={e => setNewTodo(e.target.value)} />
+        <button type="submit">Add</button>
+      </form>
+      <h2>Todo List</h2>
+      <ul>
+        {todos.map((todo) => {
+          return <Todo key={todo.id} todo={todo} />;
+        })}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ todo-list/src/Todo.jsx
@@ -0,0 +1,3 @@
+export default function Todo({ todo }) {
+  return <li>{todo.message}</li>
+}
